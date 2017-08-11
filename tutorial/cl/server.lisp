87078ba8b0d429969d955d5d7b9f29a36e6367d5
Daniel Kochmański <daniel@turtleware.eu>
2017-08-11 15:37:34 +0200

    server: add in-package

--- tutorial/cl/server.lisp
+++ tutorial/cl/server.lisp
@@ -1,4 +1,6 @@
 ;;; Needs the thrift library, shared.lisp and tutorial.lisp loaded
 
+(in-package :thrift)
+
 (let ((srv (thrift:simple-server "127.0.0.1" 9090)))
   (thrift:serve srv 'calculator-client))
