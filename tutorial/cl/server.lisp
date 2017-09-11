33ca714662a9b5ae384162364a6e8870dc09bed1
Tomek Kurcz <tomsandbox@gmail.com>
2017-09-11 15:01:38 +0200

    Fix tutorial client and server files

--- tutorial/cl/server.lisp
+++ tutorial/cl/server.lisp
@@ -1,6 +1,10 @@
 ;;; Needs the thrift library, shared.lisp and tutorial.lisp loaded
 
-(in-package :thrift)
+(in-package :thrift-generated)
+
+(defmethod add ((h handler) num1 num2)
+  (format t "~&Asked to add ~A and ~A" num1 num2)
+  (+ num1 num2))
 
 (let ((srv (thrift:simple-server "127.0.0.1" 9090)))
   (thrift:serve srv 'calculator-client))
