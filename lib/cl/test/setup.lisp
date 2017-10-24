391b810c3b2fa26b34b080a02a850f3baed6e25e
Tomek Kurcz <tomsandbox@gmail.com>
2017-10-24 13:45:23 +0200

    CL self-tests: Move setup tests to Fiasco
    Also, move them to a separate file. No one likes clutter. Or at
    least no one should.
    
    Seriously.

---
+++ lib/cl/test/setup.lisp
@@ -0,0 +1,20 @@
+(fiasco:define-test-package (#:setup-tests :in thrift-test:thrift-self-tests))
+
+(in-package #:setup-tests)
+
+(deftest thrift-class ()
+  (let ((class (find-class 'thrift-test::test-struct)))
+    (is (equal (thrift:class-identifier class) "TestStruct"))
+    (is (every #'(lambda (id name)
+                   (equal (thrift:field-definition-identifier
+                           (find id (thrift:class-field-definitions class)
+                                 :key #'thrift:field-definition-identifier-number))
+                          name))
+               '(1 2)
+               '("fieldOne" "fieldTwo")))))
+
+(deftest test-transport ()
+  (is (typep (thrift-test::make-test-transport) 'thrift:binary-transport)))
+
+(deftest test-protocol ()
+  (is (typep (thrift-test::make-test-protocol) 'thrift:binary-protocol)))
