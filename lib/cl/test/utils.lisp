515cd4cf4538fe9569788f3ea535b18b4280a644
Tomek Kurcz <tomsandbox@gmail.com>
2017-10-25 10:01:58 +0200

    CL self-tests: Remove obsolete code (old testing framework)

--- lib/cl/test/utils.lisp
+++ lib/cl/test/utils.lisp
@@ -17,88 +17,11 @@
 
 (in-package #:thrift-test-utils)
 
-(defparameter *test-root-pathname*
-  (make-pathname :name nil :type nil :defaults (or *compile-file-pathname* *load-pathname*)))
-
-(defvar *tests* (make-hash-table))
-
 (defvar *test-location* #u"thrift://127.0.0.1:9091")
 
 (defvar *test-service* (make-instance 'service :identifier "Test Root"))
 
 (defvar *test-server-process* nil)
-
-(defvar *test-break-on-errors* t)
-
-(defun find-test (name) (gethash name *tests*))
-
-(defun (setf find-test) (test-function name)
-  (if (null test-function)
-    (remhash name *tests*)
-    (setf (gethash name *tests*) test-function)))
-
-(defgeneric run-test (test)
-  (:method ((name symbol))
-    (let ((test-function (find-test name)))
-      (if test-function
-        (run-test test-function)
-        (warn "test not found: ~s." name))))
-  (:method ((test-function function))))
-
-(defun run-tests (&rest test-names)
-  (let ((succeeded 0)
-        (failed ())
-        (errored ()))
-    (flet ((run-test (test-function)
-             (multiple-value-bind (result condition name form)
-                                  (funcall test-function)
-               (cond (result
-                      (incf succeeded))
-                     (condition
-                      (format *trace-output* "~%test (~a) signaled:~%~:w~%~a" name form condition)
-                      (push name errored))
-                     (t
-                      (format *trace-output* "~&~%test (~a) failed:~%~:w" name form)
-                      (push name failed))))))
-      (if test-names
-        (dolist (pattern test-names)
-          (etypecase pattern
-            (symbol
-             (run-test (or (find-test pattern) (error "test not found: ~s." pattern))))
-            (string
-             (let ((scanner (ppcre:create-scanner (string-upcase pattern))))
-               (flet ((run-if-matched (name function)
-                        (let* ((namestring (string name))
-                               (matched-string (cl-ppcre:scan-to-strings scanner namestring)))
-                          (when (string-equal namestring matched-string)
-                            (run-test function)))))
-                 (maphash #'run-if-matched *tests*))))))
-        (loop for test being each hash-value of *tests* do (run-test test))))
-    `(,(or test-names ".*")
-      ,(if (or failed errored) :count :succeeded) ,(+ succeeded (length failed) (length errored))
-      ,@(when failed `(:failed (,(length failed) ,@failed)))
-      ,@(when errored `(:errored (,(length errored) ,@errored))))))
-
-(defmacro test (name form)
-  `(progn (setf (find-test ',name)
-                #'(lambda (&aux (name ',name) (form ',form))
-                    (multiple-value-bind (result error)
-                                         (block :do-test
-                                           (handler-bind ((error (lambda (c)
-                                                                   (when *test-break-on-errors*
-                                                                     (break "~%~a signaled ~a." ',name c))
-                                                                   (return-from :do-test (values nil c)))))
-                                             ,form))
-                      (cond (error
-                             (values nil error name form))
-                            (result
-                             (values result nil name form))
-                            (t
-                             (values nil nil name form))))))
-          ',name))
-
-#+digitool
-(setf (ccl:assq 'test ccl:*fred-special-indent-alist*) 1)
 
 (def-struct "TestStruct"
     (("fieldOne" nil :id 1 :type string)
@@ -120,9 +43,9 @@
   (apply #'make-instance 'vector-stream-transport initargs))
 
 (defun make-test-protocol (&rest initargs &key
-                                 (direction :io)
-                                 (input-transport (make-test-transport))
-                                 (output-transport input-transport))
+                                            (direction :io)
+                                            (input-transport (make-test-transport))
+                                            (output-transport input-transport))
   (apply #'make-instance 'binary-protocol
          :direction direction
          :input-transport input-transport
@@ -154,7 +77,7 @@
     protocol)
 
   (:method ((stream vector-stream))
-    (fill (thrift.implementation::get-vector-stream-vector stream) 0)
+    (fill thrift.implementation::get-vector-stream-vector stream 0)
     stream))
 
 (defun test-server (&optional (location *test-location*))
