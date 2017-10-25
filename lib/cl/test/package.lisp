47e9a09db21d1cab352ded4ae056bd5b47d14ca0
Tomek Kurcz <tomsandbox@gmail.com>
2017-10-25 07:42:51 +0200

    CL self-tests: Reorganize

--- lib/cl/test/package.lisp
+++ lib/cl/test/package.lisp
@@ -1,9 +1,13 @@
 (in-package #:common-lisp-user)
 
-
 (defpackage #:thrift-test
+  (:use #:common-lisp)
+  (:export #:thrift-self-tests))
+
+(defpackage #:thrift-test-utils
   (:shadowing-import-from #:thrift #:byte #:set #:list #:map #:type-of #:float)
-  (:use #:common-lisp #:thrift)
+  (:use #:thrift
+        #:cl)
   (:import-from #:trivial-gray-streams
                 #:stream-write-byte
                 #:stream-read-byte
@@ -11,18 +15,27 @@
                 #:stream-write-sequence
                 #:stream-force-output
                 #:stream-finish-output)
-  #+ccl
-  (:import-from #:ccl
-                #:stream-tyo
-                #:stream-tyi
-                #:stream-reader
-                #:stream-writer
-                #:stream-position)
-  #+sbcl
-  (:export #:test
-           #:with-test-services
-           #:*test-location*
-           #:thrift-self-tests))
+  (:export #:field-three
+           #:make-test-large-struct
+           #:make-test-protocol
+           #:make-test-struct
+           #:make-test-transport
+           #:reset
+           #:rewind
+           #:stream-write-byte
+           #:stream-read-byte
+           #:stream-read-sequence
+           #:stream-write-sequence
+           #:stream-force-output
+           #:stream-finish-output
+           #:test-large-struct
+           #:test-large-struct-field-one
+           #:test-large-struct-field-two
+           #:test-large-struct-field-three
+           #:test-struct
+           #:test-struct-field-one
+           #:test-struct-field-two
+           #:with-test-services))
 
 (defpackage #:thrift-test-request (:use))
 
