f91b3ff45727ce15403374bd1d5b35f05ef8e728
Tomek Kurcz <tomsandbox@gmail.com>
2017-10-26 12:47:38 +0200

    Cosmetic: self-tests: license and comments
    Some stuff that's been forgotten in the last cosmetic commit.

--- lib/cl/test/conditions.lisp
+++ lib/cl/test/conditions.lisp
@@ -1,4 +1,18 @@
-;;;; this file defines tests for exception classes.
+;;;; Copyright 2010 James Anderson <james.anderson@setf.de>
+;;;;
+;;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;;; you may not use this file except in compliance with the License.
+;;;; You may obtain a copy of the License at
+;;;;
+;;;;     http://www.apache.org/licenses/LICENSE-2.0
+;;;;
+;;;; Unless required by applicable law or agreed to in writing, software
+;;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;;; See the License for the specific language governing permissions and
+;;;; limitations under the License.
+
+;;;; This file defines tests for exception classes.
 
 (fiasco:define-test-package (#:condition-tests :in thrift-test:thrift-self-tests)
   (:use #:thrift-test-utils))
