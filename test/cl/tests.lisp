3bc88c408190e0b694b67c3f6c1400059d9c0c14
Tomek Kurcz <tomsandbox@gmail.com>
2017-10-27 11:54:37 +0200

    Cosmetic: license and comments

--- test/cl/tests.lisp
+++ test/cl/tests.lisp
@@ -1,9 +1,24 @@
-;;; The tests here only make sense in the context of a TestServer
-;;; running and the dynamic variable thrift-cross::*prot*
-;;; being set with a client connection to the TestServer. Normally,
-;;; this is handled in make-test-client.lisp.
-
 (in-package #:thrift-cross)
+
+;;;; Copyright 2017 Rigetti Computing <rigetti.com>
+;;;;
+;;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;;; you may not use this file except in compliance with the License.
+;;;; You may obtain a copy of the License at
+;;;;
+;;;;     http://www.apache.org/licenses/LICENSE-2.0
+;;;;
+;;;; Unless required by applicable law or agreed to in writing, software
+;;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;;; See the License for the specific language governing permissions and
+;;;; limitations under the License.
+
+;;;; The tests here only make sense in the context of a TestServer
+;;;; running and the dynamic variable thrift-cross::*prot*
+;;;; being set with a client connection to the TestServer. Normally,
+;;;; this is handled in make-test-client.lisp.
+
 
 ;;; Standard Thrift cross-test error codes
 (defparameter *test_basetypes* 1)
